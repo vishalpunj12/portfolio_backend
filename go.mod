730c24c2ee6fc9096fed7340a6ed645fc195eb67
B0290077 <vishal.punj@airtel.com>
2025-04-07 21:47:57 +0530

    mail api

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/vishalpunj12/protfolio_backend
+
+go 1.23.2
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/smtp"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type ContactRequest struct {
+	Name    string `json:"name"`
+	Address string `json:"address"`
+	Message string `json:"message"`
+}
+
+func main() {
+	godotenv.Load()
+	http.HandleFunc("/send", handleSend)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	fmt.Println("Server running on port", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func handleSend(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req ContactRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	err = sendEmail(req)
+	if err != nil {
+		http.Error(w, "Failed to send email: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Email sent successfully"))
+}
+
+func sendEmail(data ContactRequest) error {
+	from := os.Getenv("EMAIL_FROM")
+	password := os.Getenv("EMAIL_PASS")
+	to := os.Getenv("EMAIL_TO")
+
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
+
+	msg := []byte(
+		"To: " + to + "\r\n" +
+			"Subject: New Contact Message\r\n" +
+			"\r\n" +
+			"Name: " + data.Name + "\n" +
+			"Address: " + data.Address + "\n" +
+			"Message: " + data.Message + "\n")
+
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+	return smtp.SendMail(smtpHost+":"+smtpPort, auth, from, []string{to}, msg)
+}
